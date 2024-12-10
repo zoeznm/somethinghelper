3c26eb3f9427eaaaff10a842902369976e1e8b77
zoeznm <bomikim046@gmail.com>
2024-12-10 16:11:07 +0900

    백엔드 : 요청 처리하기

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('data')
+  getData() {
+    return { message: 'Data fetched successfully' };
+  }
+  @Post('data')
+  postData(@Body() data: any) {
+    console.log('Received data:', data);
+    return { message: 'Data received successfully' };
   }
 }
