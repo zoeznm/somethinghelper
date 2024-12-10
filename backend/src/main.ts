9d0d109cb579283cf3ca47adcefe241ac9c89e2e
zoeznm <bomikim046@gmail.com>
2024-12-10 17:09:25 +0900

    500 오류 수정 중

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // CORS 세부 설정
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
