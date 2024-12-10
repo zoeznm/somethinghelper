0097feecf535a67b5d863fd475a0056b75819887
zoeznm <bomikim046@gmail.com>
2024-12-10 15:27:34 +0900

    프론트엔드 : API 호출 코드 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // 백엔드 API 호출
+    axios.get('http://localhost:3000/')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>{message}</h1>
+  </div>
   );
 }
 
